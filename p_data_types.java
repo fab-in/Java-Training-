5d668ee66bb541838f89c3526a9af7118991d073
fabin <fabinchandi@gmail.com>
2025-10-08 19:55:52 +0530

    primitive data types

---
+++ p_data_types.java
@@ -0,0 +1,15 @@
+public class P_Data_Types {
+    public static void main(String[] args) {
+
+        byte b = 100;              // 1 byte
+        short s = 30000;           // 2 bytes
+        int i = 100000;            // 4 bytes
+        long l = 10000000000L;     // 8 bytes
+
+        float f = 3.14f;           // 4 bytes
+        double d = 3.14159265359;  // 8 bytes
+
+        char c = 'A';              // 2 bytes (Unicode character)
+        boolean flag = true;
+    }
+}
