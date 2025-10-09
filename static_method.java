2459ac9fabdb921b0f3c41b31d79244b7fc9d926
fabin <fabinchandi@gmail.com>
2025-10-09 12:55:45 +0530

    static method

---
+++ static_method.java
@@ -0,0 +1,11 @@
+class Sqaured {
+    static int square(int x) {
+        return x * x;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Sqaured.square(5)); 
+    }
+}
