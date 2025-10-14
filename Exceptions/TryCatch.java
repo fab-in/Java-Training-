16887d96778d302b76a36a94683d4d0c705f542d
fabin <fabinchandi@gmail.com>
2025-10-14 10:30:43 +0530

    MULTIPLE catch Block

--- Exceptions/TryCatch.java
+++ Exceptions/TryCatch.java
@@ -5,15 +5,16 @@ import java.util.*;
 public class TryCatch {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number 1: ");
-        int a = sc.nextInt();
-        System.out.print("Enter number 2: ");
-        int b = sc.nextInt();
+
         try {
+            System.out.print("Enter number 1: ");
+            int a = sc.nextInt();
+            System.out.print("Enter number 2: ");
+            int b = sc.nextInt();
             int val = a / b;
             System.out.println("Result: " + val);
-        } catch (ArithmeticException e) {
-            System.out.println("Denominator cannot be zero.");
+        } catch (ArithmeticException | InputMismatchException e) {
+            System.out.println("Error is : " + e);
         } finally {
             System.out.println("Execution completed.");
 
