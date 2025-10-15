7479031991bc509438dadc131725547bac7be520
fabin <fabinchandi@gmail.com>
2025-10-16 01:10:36 +0530

    project understanding dependency injection in SpringBoot

---
+++ DI/src/main/java/com/example/DI/Dev.java
@@ -0,0 +1,11 @@
+package com.example.DI;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+
+    public void build() {
+        System.out.println("Dev Build");
+    }
+}
